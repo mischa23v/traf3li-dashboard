d00d84073d3f09e122856c97b20f1c6322721bf3
Mischa23v <mischa23v@gmail.com>
2025-11-12 12:18:25 +0300

    Add translations to profile dropdown in header

--- src/components/profile-dropdown.tsx
+++ src/components/profile-dropdown.tsx
@@ -1,4 +1,5 @@
 import { Link } from '@tanstack/react-router'
+import { useTranslation } from 'react-i18next'
 import useDialogState from '@/hooks/use-dialog-state'
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import { Button } from '@/components/ui/button'
@@ -15,6 +16,7 @@ import {
 import { SignOutDialog } from '@/components/sign-out-dialog'
 
 export function ProfileDropdown() {
+  const { t } = useTranslation()
   const [open, setOpen] = useDialogState()
 
   return (
@@ -31,9 +33,9 @@ export function ProfileDropdown() {
         <DropdownMenuContent className='w-56' align='end' forceMount>
           <DropdownMenuLabel className='font-normal'>
             <div className='flex flex-col gap-1.5'>
-              <p className='text-sm leading-none font-medium'>satnaing</p>
+              <p className='text-sm leading-none font-medium'>{t('profile.dropdown.username')}</p>
               <p className='text-muted-foreground text-xs leading-none'>
-                satnaingdev@gmail.com
+                {t('profile.dropdown.email')}
               </p>
             </div>
           </DropdownMenuLabel>
@@ -41,34 +43,33 @@ export function ProfileDropdown() {
           <DropdownMenuGroup>
             <DropdownMenuItem asChild>
               <Link to='/settings'>
-                Profile
-                <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
+                {t('profile.dropdown.profile')}
+                <DropdownMenuShortcut>{t('profile.dropdown.profileShortcut')}</DropdownMenuShortcut>
               </Link>
             </DropdownMenuItem>
             <DropdownMenuItem asChild>
               <Link to='/settings'>
-                Billing
-                <DropdownMenuShortcut>⌘B</DropdownMenuShortcut>
+                {t('profile.dropdown.billing')}
+                <DropdownMenuShortcut>{t('profile.dropdown.billingShortcut')}</DropdownMenuShortcut>
               </Link>
             </DropdownMenuItem>
             <DropdownMenuItem asChild>
               <Link to='/settings'>
-                Settings
-                <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
+                {t('profile.dropdown.settings')}
+                <DropdownMenuShortcut>{t('profile.dropdown.settingsShortcut')}</DropdownMenuShortcut>
               </Link>
             </DropdownMenuItem>
-            <DropdownMenuItem>New Team</DropdownMenuItem>
+            <DropdownMenuItem>{t('profile.dropdown.newTeam')}</DropdownMenuItem>
           </DropdownMenuGroup>
           <DropdownMenuSeparator />
           <DropdownMenuItem variant='destructive' onClick={() => setOpen(true)}>
-            Sign out
+            {t('profile.dropdown.signOut')}
             <DropdownMenuShortcut className='text-current'>
-              ⇧⌘Q
+              {t('profile.dropdown.signOutShortcut')}
             </DropdownMenuShortcut>
           </DropdownMenuItem>
         </DropdownMenuContent>
       </DropdownMenu>
-
       <SignOutDialog open={!!open} onOpenChange={setOpen} />
     </>
   )
